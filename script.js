69479d85531daa901673e5a14908b8de85bba4f7
jrabasilio <jrabasilio@gmail.com>
2018-08-19 13:28:38 +0100

    remove coments in js

--- script.js
+++ script.js
@@ -3,15 +3,10 @@ console.log("Script load sucess");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    console.log("scroll Function call");
-    console.log("--> "+document.body.scrollTop);
-    console.log("--> "+document.documentElement.scrollTop);
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementById("buttonTop").style.display = "block";
-        console.log("block");
     } else {
         document.getElementById("buttonTop").style.display = "none";
-        console.log("none");
     }
 }
 
